4abd48a7d8fd39cb82be7e6535c5a1c58ab79021
Grover Toledo <grovertoledo98@gmail.com>
2022-09-25 19:19:57 -0500

    chore: add react-simple-tipewriter library

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
