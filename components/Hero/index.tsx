626145a3d7748a22d204b538d024698f928188f2
Grover Toledo <grovertoledo98@gmail.com>
2022-09-25 19:33:58 -0500

    feature: add hero buttons/ z-index/ buttons-styles

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTypewriter, Cursor} from 'react-simple-typewriter';
 import BackgroundCircles from '../BackgroundCircles';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {}
 const words = [
@@ -27,7 +28,7 @@ export default function Hero({}: Props) {
         height={128}
         className='relative rounded-full mx-auto object-cover'
       />
-      <div>
+      <div className='z-20'>
         <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
           Software engineer
         </h2>
@@ -35,6 +36,21 @@ export default function Hero({}: Props) {
           <span className='mr-3'>{text}</span>
           <Cursor cursorColor='#F7AB0A' />
         </h1>
+
+        <div className='pt-5'>
+          <Link href='#about'>
+            <button className='heroButton'>About</button>
+          </Link>
+          <Link href='#experience'>
+            <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href='#skills'>
+            <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href='#projects'>
+            <button className='heroButton'>Projects</button>
+          </Link>
+        </div>
       </div>
     </div>
   )
