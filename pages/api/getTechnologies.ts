0d2527b7b566852f12f1cb7cfa9ca92b99bb5d4f
Grover Toledo <grovertoledo98@gmail.com>
2023-01-16 17:52:50 -0500

    refactor: change skills to technologies

---
+++ pages/api/getTechnologies.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { groq } from "next-sanity";
+import { sanityClient } from "../../sanity";
+import { Technology } from "../../typings";
+
+const query = groq`
+  *[_type == "technology"]
+`
+type Data = {
+  technologies: Technology[]
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const technologies: Technology[] = await sanityClient.fetch(query);
+  res.status(200).json({ technologies })
+}
